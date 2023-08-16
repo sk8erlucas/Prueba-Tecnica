963f14269704678cc03e7e7551988e0b62673e80
sk8erlucas <galileo.laino@gmail.com>
2023-08-16 09:33:00 -0300

    modified:   PruebaTecnica/main.jsx 	modified:   PruebaTecnica/src/App.jsx 	modified:   README.md

--- PruebaTecnica/src/App.jsx
+++ PruebaTecnica/src/App.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+const CAT_FACT_ENDPOINT = 'https://catfact.ninja/fact'
 
 export function App () {
+  const [fact, setFact] = useState(null)
+  const [imageURL, setImageURL] = useState('')
+
+  useEffect(() => {
+    fetch(CAT_FACT_ENDPOINT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const fourWords = fact.split(' ', 4).join(' ')
+        fetch(`https://cataas.com/cat/says/${fourWords}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            const { url } = response
+            setImageURL(url)
+          })
+      })
+  }, [])
+
   return (
-    <h1>App de gatitos</h1>
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {imageURL && <img src={'https://cataas.com' + imageURL} alt='Cat image extracted from cataas API' />}
+    </main>
   )
 }
